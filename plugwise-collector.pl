8c1aa1c3fe239d4773865ed50a84f29f47095f97
Maxime Boutserin <maxime.boutserin@inria.fr>
2014-08-26 15:57:21 +0200

    nouvelle bdd + mode tous circles

--- plugwise-collector.pl
+++ plugwise-collector.pl
@@ -13,10 +13,12 @@ use DBI;
 # (re)initialise la base de données
 sub initbdd {
   my $dbh = connecttodb();
-  $dbh->do("DROP TABLE IF EXISTS record, circlegroup, circle");
-  $dbh->do("CREATE TABLE IF NOT EXISTS record (id INTEGER AUTO_INCREMENT UNIQUE, circleid VARCHAR(10), date DATETIME, value FLOAT, recdate DATETIME, PRIMARY KEY (circleid, date))");
-  $dbh->do("CREATE TABLE IF NOT EXISTS circlegroup (id INTEGER AUTO_INCREMENT UNIQUE, name VARCHAR(20), circleid VARCHAR(10))");
-  $dbh->do("CREATE TABLE IF NOT EXISTS circle (id INTEGER AUTO_INCREMENT UNIQUE, name VARCHAR(20), address VARCHAR(10) PRIMARY KEY, historyindex INTEGER DEFAULT -1)");
+  $dbh->do("DROP TABLE IF EXISTS record, recordset, circle, monitoredentity, childentity");
+  $dbh->do("CREATE TABLE IF NOT EXISTS record (circleaddress VARCHAR(10), date DATETIME, value FLOAT, recdate DATETIME, PRIMARY KEY (circleaddress, date))");
+  $dbh->do("CREATE TABLE IF NOT EXISTS monitoredentity (id INTEGER AUTO_INCREMENT UNIQUE PRIMARY KEY, name VARCHAR(20))");
+  $dbh->do("CREATE TABLE IF NOT EXISTS circle (name VARCHAR(20), address VARCHAR(10) PRIMARY KEY, historyindex INTEGER DEFAULT -1)");
+  $dbh->do("CREATE TABLE IF NOT EXISTS childentity (parent VARCHAR(20), child VARCHAR(20), PRIMARY KEY (parent, child))");
+  $dbh->do("CREATE TABLE IF NOT EXISTS recordset (circleaddress VARCHAR(10), monitoredentityid INTEGER, begining DATETIME, end DATETIME DEFAULT null, PRIMARY KEY (circleaddress, monitoredentityid, begining))");
   $dbh->disconnect();
 }
 
@@ -37,7 +39,7 @@ sub record {
 #  if $test->[0]->{'value'}
 
   my $dbh = connecttodb();
-  $dbh->do("INSERT INTO record (circleid, date, value, recdate) VALUES('$circle', $date, $value, $recdate);");
+  $dbh->do("INSERT INTO record (circleaddress, date, value, recdate) VALUES('$circle', $date, $value, $recdate)");
   $dbh->disconnect();
 }
 
@@ -47,7 +49,29 @@ sub createcircle {
   my $circle = shift(@_);
 
   my $dbh = connecttodb();
-  $dbh->do("INSERT INTO circle (name, address) VALUES('$circle', '$circle');");
+  $dbh->do("INSERT INTO circle (name, address) VALUES('$circle', '$circle')");
+  $dbh->disconnect();
+}
+
+# créer un nouveau recordset dans la bdd
+sub createrecordset {
+  my ($circleaddress, $monitoredentityid, $begining) = @_;
+
+# il faut des tests des variables ici, comme dans toutes ces fonctions d'ailleurs
+
+  my $dbh = connecttodb();
+  $dbh->do("INSERT INTO recordset (circleaddress, monitoredentityid, begining) VALUES('$circleaddress', $monitoredentityid, $begining)");
+  $dbh->disconnect();
+}
+
+# créer un nouveau monitoredentity dans la bdd
+sub createentity {
+  my ($name) = @_;
+
+# il faut des tests des variables ici, comme dans toutes ces fonctions d'ailleurs
+
+  my $dbh = connecttodb();
+  $dbh->do("INSERT INTO monitoredentity (name) VALUES('$name')");
   $dbh->disconnect();
 }
 
@@ -85,21 +109,57 @@ sub checkcircle {
   my $blob = selectfromdb($stmt);
 
   if(defined $blob->[0] ){
-print "$circle 's historyindex = ", $blob->[0], "\n";
+print "$circle 's historyindex = ", $$blob[0]{'historyindex'}, "\n";
 
-    return $blob->[0];
-  }else {return -2;}
+    return $$blob[0]{'historyindex'};
+  }else{
+    return -2;
+  }
 }
 
+# mettre à jour historyindex
+sub updatehistoryindex {
+  my($circle, $historyindex) = @_;
+
+  my $dbh = connecttodb();
+  $dbh->do("UPDATE circle SET historyindex = $historyindex WHERE name = '$circle'");
+  $dbh->disconnect();
+
+}
+
+# select all circle de la table circle
+sub getcircleslist {
+  my $stmt = "SELECT address FROM circle";
+
+  my $blob = selectfromdb($stmt);
+
+  if(defined $blob->[0] ){
+    return $blob;
+  }else{
+print "No circle found.";
+    return 0;
+  }
+
+
+}
 
 #################################
 
 sub convertdate {
   my $date = shift(@_);
 
-  $date =~ s/(\d\d\d\d)(\d\d)(\d\d)(\d\d)(\d\d)/"$1-$2-$3 $4:$5:00"/;
+  if(defined $date){
+    $date =~ s/(\d\d\d\d)(\d\d)(\d\d)(\d\d)(\d\d)/"$1-$2-$3 $4:$5:00"/;
+# sale exception ; il faudrait utiliser des fonction perl je pense, ou des vrai trucs mysql
+#print "date is $date\n"; 
+    if($date =~ /\d\d\d\d-07-(3[2-9]|4\d)/){
+      return "0000-00-00 00:00:00";
+    }
 
-  return $date;
+    return $date;
+  }else{
+    return "0000-00-00 00:00:00";
+  }
 }
 
 
@@ -112,19 +172,32 @@ sub convertdate {
 
 my $plugwise = Device::Plugwise->new(device => '/dev/ttyUSB0');
 
-initbdd();
+#initbdd();
 
 # début
-my $circle = '7291CD';
+## il faut faire la liste des circle à récupérer
+#my $circle = '7291CD';
+my $circles = getcircleslist();
+if($circles->[0] == 0){
+  return 0;
+}
+
+
+
+
+for my $refcircle (@$circles){
+print "circle is $$refcircle{'address'}\n";
+  my $circle = $$refcircle{'address'};
 
 ### voir le statut pour récupérer l'index
-$plugwise->command('status', $circle);
-my $message = $plugwise->read($TIMEOUT);
+  $plugwise->command('status', $circle);
+  my $message = $plugwise->read($TIMEOUT);
 #print $message->{"body"}[6], ":", $message->{"body"}[7], "\n";
-my $lastindex = $message->{"body"}[7];
+  my $lastindex = $message->{"body"}[7];
+  print "lastindex = $lastindex\n";
 ### la date doit être convertie
-my $recdate = convertdate($message->{"body"}[9]);
-#print "recdate : ", $recdate, "\n";
+  my $recdate = convertdate($message->{"body"}[9]);
+  print "recdate : ", $recdate, "\n";
 
 
 #print $message->{"schema"}, "\n";
@@ -133,30 +206,31 @@ my $recdate = convertdate($message->{"body"}[9]);
 #}
 
 ### voir la base de données pour connaître le dernier index enregistré
-my $dbindex = checkcircle($circle);
-#print "dbindex = $dbindex\n";
+  my $dbindex = checkcircle($circle);
+print "dbindex = $dbindex\n";
 
-##### si le circle n'existe pas, l'ajouter
-if($dbindex < -1){
- createcircle($circle); 
-}
+##### si le circle n'existe pas, l'ajouter ### n'a plus de sens, le circle doit s'ajouter à la main
+#  if($dbindex < -1){
+#   createcircle($circle); 
+#  }
 
 ### journaliser tous les index à enregistrer
 
 
 ### récupérer les index nécessaires
-
-$plugwise->command('history', '7291CD', $lastindex-1);
-$message = $plugwise->read($TIMEOUT);
-# foreach(@{$message->{"body"}}){
-#   print $_, "\n";
-# }
+  for(my $i = $dbindex+1;$i < $lastindex;$i++){
+    $plugwise->command('history', $circle, $i);
+    $message = $plugwise->read($TIMEOUT);
 
 ### enregistrer les index récupérés
-#print "record(", $message->{"body"}[1], ",", convertdate($message->{"body"}[9]), ",", $message->{"body"}[5], ",", $recdate, ")\n";
-record($message->{"body"}[1], convertdate($message->{"body"}[9]), $message->{"body"}[5], $recdate);
+    if(convertdate($message->{"body"}[9]) !~ /0000-00-00 00:00:00/){
+      record($message->{"body"}[1], convertdate($message->{"body"}[9]), $message->{"body"}[5], $recdate);
+    }
+  }
 
 ### mettre à jour circle.historyindex
+  updatehistoryindex($circle, $lastindex-1);
+}
 
 # fin
 
